c5e46e9c58bb5f5769e3101fa2eba8da81126e44
Hamid Rafiq <hamidrafiq962@gmail.com>
2025-06-18 17:46:49 +0500

    fix: build issues

--- src/app/(pages)/dashboard/[id]/page.tsx
+++ src/app/(pages)/dashboard/[id]/page.tsx
@@ -1,14 +1,28 @@
-import LeadDetails from "@/views/Lead-Details";
-import React from "react";
-// import { PageProps } from "next";
-type PageProps = {
-  params: { id: string };
-};
-// const LeadDetailsPage = ({ params }: { params: { id: string } }) => {
-export default function LeadDetailsPage({
-  params,
-}: PageProps & { params: { id: string } }) {
-  const { id } = params;
+// import LeadDetails from "@/views/Lead-Details";
+// import React from "react";
+// // import { PageProps } from "next";
+// type PageProps = {
+//   params: { id: string };
+// };
+// // const LeadDetailsPage = ({ params }: { params: { id: string } }) => {
+// export default function LeadDetailsPage({
+//   params,
+// }: PageProps & { params: { id: string } }) {
+//   const { id } = params;
 
-  return <LeadDetails leadId={id} />;
+//   return <LeadDetails leadId={id} />;
+// }
+// app/dashboard/[id]/page.tsx or similar path
+
+import React from "react";
+import LeadDetails from "@/views/Lead-Details";
+
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+export default function LeadDetailsPage({ params }: Props) {
+  return <LeadDetails leadId={params.id} />;
 }
